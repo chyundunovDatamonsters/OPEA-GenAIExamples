7a7dc5ec36b8e125f563e614311eb51cc87a954e
Chingis Yundunov <YundunovCN@sibedge.com>
2024-11-19 19:08:46 +0700

    VodeQnA application - Fix files for deploy VideoQnA app on AMD GPU
    Signed-off-by: Chingis Yundunov <YundunovCN@sibedge.com>

--- VideoQnA/docker_compose/amd/gpu/rocm/set_env.sh
+++ VideoQnA/docker_compose/amd/gpu/rocm/set_env.sh
@@ -9,15 +9,15 @@ export VIDEOQNA_RERANK_SERVICE_HOST_IP=${host_ip}
 export VIDEOQNA_LVM_SERVICE_HOST_IP=${host_ip}
 
 export VIDEOQNA_MODEL_ID="DAMO-NLP-SG/VideoLLaMA2.1-7B-16F"
-export VIDEOQNA_LVM_ENDPOINT="http://${host_ip}:8081"
-export VIDEOQNA_BACKEND_SERVICE_ENDPOINT="http://${host_ip}:8888/v1/videoqna"
-export VIDEOQNA_BACKEND_HEALTH_CHECK_ENDPOINT="http://${host_ip}:8888/v1/health_check"
-export VIDEOQNA_DATAPREP_SERVICE_ENDPOINT="http://${host_ip}:6007/v1/dataprep"
-export VIDEOQNA_DATAPREP_GET_FILE_ENDPOINT="http://${host_ip}:6007/v1/dataprep/get_file"
-export VIDEOQNA_DATAPREP_GET_VIDEO_LIST_ENDPOINT="http://${host_ip}:6007/v1/dataprep/get_videos"
+export VIDEOQNA_LVM_ENDPOINT="http://${host_ip}:19009"
+export VIDEOQNA_BACKEND_SERVICE_ENDPOINT="http://${host_ip}:18888/v1/videoqna"
+export VIDEOQNA_BACKEND_HEALTH_CHECK_ENDPOINT="http://${host_ip}:18888/v1/health_check"
+export VIDEOQNA_DATAPREP_SERVICE_ENDPOINT="http://${host_ip}:16007/v1/dataprep"
+export VIDEOQNA_DATAPREP_GET_FILE_ENDPOINT="http://${host_ip}:16007/v1/dataprep/get_file"
+export VIDEOQNA_DATAPREP_GET_VIDEO_LIST_ENDPOINT="http://${host_ip}:16007/v1/dataprep/get_videos"
 
 export VIDEOQNA_VDMS_HOST=${host_ip}
-export VIDEOQNA_VDMS_PORT=8001
+export VIDEOQNA_VDMS_PORT=18001
 export VIDEOQNA_INDEX_NAME="mega-videoqna"
 export VIDEOQNA_USECLIP=1
 export VIDEOQNA_LLM_DOWNLOAD="True" # Set to "False" before redeploy LVM server to avoid model download

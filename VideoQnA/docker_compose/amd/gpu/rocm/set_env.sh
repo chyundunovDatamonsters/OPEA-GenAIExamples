a6259ed1d6dbd0cacea54194a3e647b5807fbf6e
Chingis Yundunov <YundunovCN@sibedge.com>
2024-11-19 16:19:48 +0700

    VodeQnA application - Fix files for deploy VideoQnA app on AMD GPU
    Signed-off-by: Chingis Yundunov <YundunovCN@sibedge.com>

--- VideoQnA/docker_compose/amd/gpu/rocm/set_env.sh
+++ VideoQnA/docker_compose/amd/gpu/rocm/set_env.sh
@@ -2,21 +2,21 @@
 
 host_ip=$(hostname -I | awk '{print $1}')
 
-export MEGA_SERVICE_HOST_IP=${host_ip}
-export EMBEDDING_SERVICE_HOST_IP=${host_ip}
-export RETRIEVER_SERVICE_HOST_IP=${host_ip}
-export RERANK_SERVICE_HOST_IP=${host_ip}
-export LVM_SERVICE_HOST_IP=${host_ip}
+export VIDEOQNA_MEGA_SERVICE_HOST_IP=${host_ip}
+export VIDEOQNA_EMBEDDING_SERVICE_HOST_IP=${host_ip}
+export VIDEOQNA_RETRIEVER_SERVICE_HOST_IP=${host_ip}
+export VIDEOQNA_RERANK_SERVICE_HOST_IP=${host_ip}
+export VIDEOQNA_LVM_SERVICE_HOST_IP=${host_ip}
 
-export LVM_ENDPOINT="http://${host_ip}:9009"
-export BACKEND_SERVICE_ENDPOINT="http://${host_ip}:8888/v1/videoqna"
-export BACKEND_HEALTH_CHECK_ENDPOINT="http://${host_ip}:8888/v1/health_check"
-export DATAPREP_SERVICE_ENDPOINT="http://${host_ip}:6007/v1/dataprep"
-export DATAPREP_GET_FILE_ENDPOINT="http://${host_ip}:6007/v1/dataprep/get_file"
-export DATAPREP_GET_VIDEO_LIST_ENDPOINT="http://${host_ip}:6007/v1/dataprep/get_videos"
+export VIDEOQNA_LVM_ENDPOINT="http://${host_ip}:9009"
+export VIDEOQNA_BACKEND_SERVICE_ENDPOINT="http://${host_ip}:8888/v1/videoqna"
+export VIDEOQNA_BACKEND_HEALTH_CHECK_ENDPOINT="http://${host_ip}:8888/v1/health_check"
+export VIDEOQNA_DATAPREP_SERVICE_ENDPOINT="http://${host_ip}:6007/v1/dataprep"
+export VIDEOQNA_DATAPREP_GET_FILE_ENDPOINT="http://${host_ip}:6007/v1/dataprep/get_file"
+export VIDEOQNA_DATAPREP_GET_VIDEO_LIST_ENDPOINT="http://${host_ip}:6007/v1/dataprep/get_videos"
 
-export VDMS_HOST=${host_ip}
-export VDMS_PORT=8001
-export INDEX_NAME="mega-videoqna"
-export USECLIP=1
-export LLM_DOWNLOAD="True" # Set to "False" before redeploy LVM server to avoid model download
+export VIDEOQNA_VDMS_HOST=${host_ip}
+export VIDEOQNA_VDMS_PORT=8001
+export VIDEOQNA_INDEX_NAME="mega-videoqna"
+export VIDEOQNA_USECLIP=1
+export VIDEOQNA_LLM_DOWNLOAD="True" # Set to "False" before redeploy LVM server to avoid model download

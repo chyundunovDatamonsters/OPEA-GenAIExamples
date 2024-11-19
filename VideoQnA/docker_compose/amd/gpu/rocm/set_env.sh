181025e1f35541c018014e19a52588fbf1132ae0
Chingis Yundunov <YundunovCN@sibedge.com>
2024-11-19 19:04:22 +0700

    VodeQnA application - Fix files for deploy VideoQnA app on AMD GPU
    Signed-off-by: Chingis Yundunov <YundunovCN@sibedge.com>

--- VideoQnA/docker_compose/amd/gpu/rocm/set_env.sh
+++ VideoQnA/docker_compose/amd/gpu/rocm/set_env.sh
@@ -8,8 +8,8 @@ export VIDEOQNA_RETRIEVER_SERVICE_HOST_IP=${host_ip}
 export VIDEOQNA_RERANK_SERVICE_HOST_IP=${host_ip}
 export VIDEOQNA_LVM_SERVICE_HOST_IP=${host_ip}
 
-export VIDEOQNA_MODEL_ID="OpenFace-CQUPT/Human_LLaVA"
-export VIDEOQNA_LVM_ENDPOINT="http://${host_ip}:9009"
+export VIDEOQNA_MODEL_ID="DAMO-NLP-SG/VideoLLaMA2.1-7B-16F"
+export VIDEOQNA_LVM_ENDPOINT="http://${host_ip}:8081"
 export VIDEOQNA_BACKEND_SERVICE_ENDPOINT="http://${host_ip}:8888/v1/videoqna"
 export VIDEOQNA_BACKEND_HEALTH_CHECK_ENDPOINT="http://${host_ip}:8888/v1/health_check"
 export VIDEOQNA_DATAPREP_SERVICE_ENDPOINT="http://${host_ip}:6007/v1/dataprep"

9461cdb4ac3feec5f0c2dc42a4d1a25cd9d4f73e
Chingis Yundunov <YundunovCN@sibedge.com>
2024-11-22 00:40:50 +0700

    AudioQnA - add files for deploy AudioQnA application on AMD with vLLM service
    Signed-off-by: Chingis Yundunov <YundunovCN@sibedge.com>

---
+++ AudioQnA/docker_compose/amd/gpu/rocm-vllm/set_env.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash                                                                                                           set_env.sh
+
+# Copyright (C) 2024 Advanced Micro Devices, Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+
+# export host_ip=<your External Public IP>    # export host_ip=$(hostname -I | awk '{print $1}')
+
+export host_ip="192.165.1.21"
+export AUDIOQNA_HUGGINGFACEHUB_API_TOKEN=${HUGGINGFACEHUB_API_TOKEN}
+# <token>
+
+export AUDIOQNA_VLLM_ENDPOINT=http://$host_ip:3006
+export AUDIOQNA_LLM_MODEL_ID=Intel/neural-chat-7b-v3-3
+
+export AUDIOQNA_ASR_ENDPOINT=http://$host_ip:7066
+export AUDIOQNA_TTS_ENDPOINT=http://$host_ip:7055
+
+export AUDIOQNA_MEGA_SERVICE_HOST_IP=${host_ip}
+export AUDIOQNA_ASR_SERVICE_HOST_IP=${host_ip}
+export AUDIOQNA_TTS_SERVICE_HOST_IP=${host_ip}
+export AUDIOQNA_LLM_SERVICE_HOST_IP=${host_ip}
+
+export AUDIOQNA_ASR_SERVICE_PORT=3001
+export AUDIOQNA_TTS_SERVICE_PORT=3002
+export AUDIOQNA_LLM_SERVICE_PORT=3007

ce37e9ce7af7d0df7057c3e40d15ca64e367bd8a
Chingis Yundunov <YundunovCN@sibedge.com>
2024-11-22 14:27:03 +0700

    AudioQnA - fix Docker compose file, set envs script and adding tests script file
    Signed-off-by: Chingis Yundunov <YundunovCN@sibedge.com>

--- AudioQnA/docker_compose/amd/gpu/rocm-vllm/set_env.sh
+++ AudioQnA/docker_compose/amd/gpu/rocm-vllm/set_env.sh
@@ -9,7 +9,7 @@
 export host_ip="192.165.1.21"
 export AUDIOQNA_HUGGINGFACEHUB_API_TOKEN=${HUGGINGFACEHUB_API_TOKEN}
 export AUDIOQNA_VLLM_ENDPOINT=http://$host_ip:3006/v1/chat/completions
-export AUDIOQNA_LLM_SERVICE_ENDPOINT=http://$host_ip:3007/v1/chat/completions
+export AUDIOQNA_LLM_SERVICE_ENDPOINT=http://$host_ip:3006/v1/chat/completions
 export AUDIOQNA_LLM_MODEL_ID=meta-llama/Meta-Llama-3-8B-Instruct
 export AUDIOQNA_ASR_ENDPOINT=http://$host_ip:7066
 export AUDIOQNA_TTS_ENDPOINT=http://$host_ip:7055
@@ -19,4 +19,4 @@ export AUDIOQNA_TTS_SERVICE_HOST_IP=${host_ip}
 export AUDIOQNA_LLM_SERVICE_HOST_IP=${host_ip}
 export AUDIOQNA_ASR_SERVICE_PORT=3001
 export AUDIOQNA_TTS_SERVICE_PORT=3002
-export AUDIOQNA_LLM_SERVICE_PORT=3007
+export AUDIOQNA_LLM_SERVICE_PORT=3006

256893017185ea52f8b82f1f83cef5c4ac7a5ba0
Chingis Yundunov <YundunovCN@sibedge.com>
2024-12-09 12:44:47 +0700

    DocSum - fix docke compose file and set envs script
    Signed-off-by: Chingis Yundunov <YundunovCN@sibedge.com>

--- DocSum/docker_compose/amd/gpu/rocm/set_env.sh
+++ DocSum/docker_compose/amd/gpu/rocm/set_env.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# Copyright (C) 2024 Advanced Micro Devices, Inc.
 # SPDX-License-Identifier: Apache-2.0
 
 export DOCSUM_TGI_IMAGE="ghcr.io/huggingface/text-generation-inference:2.3.1-rocm"
@@ -13,3 +12,9 @@ export DOCSUM_LLM_SERVER_PORT="9000"
 export DOCSUM_BACKEND_SERVER_PORT="8888"
 export DOCSUM_FRONTEND_PORT="5173"
 export BACKEND_SERVICE_ENDPOINT="http://${HOST_IP}:${DOCSUM_BACKEND_SERVER_PORT}/v1/docsum"
+export V2A_SERVICE_HOST_IP=${host_ip}
+export V2A_ENDPOINT=http://$host_ip:7078
+export A2T_ENDPOINT=http://$host_ip:7066
+export A2T_SERVICE_HOST_IP=${host_ip}
+export A2T_SERVICE_PORT=9099
+

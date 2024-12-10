a6b360b21849f5cafeac8c3292e8da42ff4a4770
Chingis Yundunov <YundunovCN@sibedge.com>
2024-12-10 12:46:32 +0700

    DocSum - fix UI FaqGen
    Signed-off-by: Chingis Yundunov <YundunovCN@sibedge.com>

--- ProductivitySuite/ui/react/src/components/FaqGen/FaqGen.tsx
+++ ProductivitySuite/ui/react/src/components/FaqGen/FaqGen.tsx
@@ -68,7 +68,7 @@ const FaqGen = () => {
     fetchEventSource(FAQ_GEN_URL, {
         method: "POST",
         headers: {
-            "Content-Type": "undefined",
+            "Content-Type": "multipart/form-data",
             "Accept": "*/*"
         },
         body: formData,

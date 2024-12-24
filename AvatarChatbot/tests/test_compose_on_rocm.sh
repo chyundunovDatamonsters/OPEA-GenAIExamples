a0bfa30f8a79dc3009c28b79841994e0453fbdd4
Chingis Yundunov <YundunovCN@sibedge.com>
2024-12-24 20:24:57 +0700

    ProductivitySuite - fix deploy on AMD
    Signed-off-by: Chingis Yundunov <YundunovCN@sibedge.com>

--- AvatarChatbot/tests/test_compose_on_rocm.sh
+++ AvatarChatbot/tests/test_compose_on_rocm.sh
@@ -130,7 +130,7 @@ function main() {
     stop_docker
     if [[ "$IMAGE_REPO" == "opea" ]]; then build_docker_images; fi
     start_services
-    sleep 10
+    sleep 30
     # validate_microservices
     validate_megaservice
     # validate_frontend

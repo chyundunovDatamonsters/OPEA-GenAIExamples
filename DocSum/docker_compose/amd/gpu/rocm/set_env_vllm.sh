459505f5af219a79c759fed62d9ab4715b5ddbe1
Chingis Yundunov <YundunovCN@sibedge.com>
2025-01-13 11:22:41 +0700

    DocSum - add files for deploy on ROCm with vLLM
    Signed-off-by: Chingis Yundunov <YundunovCN@sibedge.com>

---
+++ DocSum/docker_compose/amd/gpu/rocm/set_env_vllm.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Copyright (C) 2024 Advanced Micro Devices, Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+export MAX_INPUT_TOKENS=2048
+export MAX_TOTAL_TOKENS=4096
+export DOCSUM_VLLM_SERVICE_PORT=8081
+export DOCSUM_LLM_MODEL_ID="Intel/neural-chat-7b-v3-3"
+export HOST_IP=${host_ip}
+export DOCSUM_VLLM_ENDPOINT="http://${HOST_IP}:${DOCSUM_VLLM_SERVICE_PORT}"
+export DOCSUM_HUGGINGFACEHUB_API_TOKEN=${your_hf_api_token}
+export DOCSUM_LLM_SERVER_PORT="9000"
+export DOCSUM_BACKEND_SERVER_PORT="8888"
+export DOCSUM_FRONTEND_PORT="5173"
+export BACKEND_SERVICE_ENDPOINT="http://${HOST_IP}:${DOCSUM_BACKEND_SERVER_PORT}/v1/docsum"
